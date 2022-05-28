677f55b7b63b83421faa1d021778c0682757d79c
Zelda <lumakawar@hotmail.com>
2022-05-28 17:41:23 -0400

    finalupload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # WorkDayScheduler
+
+A dynamic schedule that saves text inputs and also color codes the time depending on if the planner event is in the past, present or future as indicated by the colors: cyan, orange and green.
+
+
+Deployment link:
+
+
+
+Screen shots:
+
+![alt text](assets/images/screenshot1.png)
+![alt text](assets/images/screenshot2.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,58 @@
 $(document).ready(function () {
+    
     // sets current date
 let today = moment().format("ddd MMM Do, YYYY");
 $("#currentDay").text(today);
 let currentHour = moment().format("HH");
+console.log(currentHour)
 
     
-    $(".time-div").each(function () {
-        var timeDiv = $(this).attr("id").split("-")[1];
-        
-        if (currentHour == timeDiv) {
-          $(this).addClass("present");
-          $(this).children(".description").addClass("white-text");
-        } else if (currentHour < timeDiv) {
-          $(this).removeClass("present");
-          $(this).addClass("future");
-        } else if (currentHour > timeDiv) {
-          $(this).removeClass("future");
-          $(this).addClass("past");
-        }
-      });
+   // compares current time with schedule and colors accorindingly
+
+    var timeDiv = document.querySelectorAll(".time-div")
+
+    timeDiv.forEach(item=>{
+
+        let time = item.id
+        if (currentHour == time) {
+                 item.classList.add("present");    
+                } else if (currentHour < time) {
+                  item.classList.remove("present");
+                  item.classList.add("future");
+                } else if (currentHour > time) {
+                  item.classList.remove("future");
+                  item.classList.add("past");
+                }
+    })
 
 
 
-//saves items to local storage
-$(".saveBtn").click(function (event) {
-    event.preventDefault();
-    var value = $(this).siblings(".time-block").val();
-    var time = $(this).parent().attr("id").split("-")[1];
-    localStorage.setItem(time, value);
-  });
+//saves and retrives items to and from local storage
+let data = []
 
-  //retrieves items from local storage 
-  $("9th-hour .time-block").val(localStorage.getItem("09"));
-  $("#10th-hour .time-block").val(localStorage.getItem("10"));
-  $("#11th-hour .time-block").val(localStorage.getItem("11"));
-  $("#12th-hour .time-block").val(localStorage.getItem("12"));
-  $("#13th-hour .time-block").val(localStorage.getItem("13"));
-  $("#14th-hour .time-block").val(localStorage.getItem("14"));
-  $("#15th-hour .time-block").val(localStorage.getItem("15"));
-  $("#16th-hour .time-block").val(localStorage.getItem("16"));
-  $("#17th-hour .time-block").val(localStorage.getItem("17"));
+timeDiv.forEach(item=>{
+   item.addEventListener("submit", (e)=>{
+       e.preventDefault()
+      
+       data.push(item.description.value)
+
+       console.log(data)
+       localStorage.setItem("data", JSON.stringify(data))
+   })
+})
+
+let retrieved = JSON.parse(localStorage.getItem("data"))
+console.log(retrieved)
+document.querySelectorAll(".description").forEach((item, i)=>{
+   if(retrieved[i]){
+       item.value = retrieved[i]
+   }else{
+       item.value = ""
+   }
+})
+
+
+
 
 });
 
